f6cd3f9023ff854b8a4db70eb86278a51c60d945
aad <aguahidol@alu.edu.gva.es>
2024-01-11 20:50:04 +0100

    a

--- src/Set1.java
+++ src/Set1.java
@@ -1,6 +1,4 @@
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 public class Set1 {
     public static void main(String[] args) {
@@ -27,11 +25,20 @@ public class Set1 {
         System.out.println(set);
          */
 
-        printWithoutDuplicates("a", "b", "c", "a", "c", "b");
-
-
-
-
+        printWithoutDuplicates("a", "b", "a", "c", "b", "a", "d");
+        printDuplicates("a", "b", "a", "c", "b", "a", "d");
+        printDuplicates2("a", "b", "a", "c", "b", "a", "d");
+        Set<String> set1 = new HashSet<>();
+        set1.add("a");
+        set1.add("b");
+        set1.add("c");
+        Set<String> set2 = new HashSet<>();
+        set2.add("b");
+        set2.add("c");
+        set2.add("d");
+        Set<String> result = new HashSet<>();
+        System.out.println(getIntersection(set1, set2));
+        System.out.println(getIntersection2(set1, set2));
     }
 
     // Exercise 1
@@ -69,9 +76,46 @@ public class Set1 {
         for(String s: array) {
             newSet.add(s);
         }
-
         System.out.println(newSet);
+    }
 
+    public static void printDuplicates(String... array) {
+        Set<String> setTemp = new HashSet<String>();
+        Set<String> setDuplicates= new HashSet<String>();
+        for(String s : array) {
+            if (!setTemp.add(s)) {
+                setDuplicates.add(s);
+            }
+        }
+        System.out.println(setDuplicates);
+    }
+
+    public static void printDuplicates2(String... array) {
+        List<String> list = new ArrayList<String>(Arrays.asList(array));
+        Set<String> set = new HashSet<String>(list);
+        for(String s : set) {
+            list.remove(s);
+        }
+        Set<String> setDuplicates = new HashSet<>(list);
+        System.out.println(setDuplicates);
+    }
+
+    public static Set<String> getIntersection(Set<String> set1, Set<String> set2) {
+        Set<String> result = new HashSet<>(set1);
+        result.retainAll(set2);
+        return result;
+
+    }
+
+    public static Set<String> getIntersection2(Set<String> set1, Set<String> set2) {
+        Set<String> temp = new HashSet<>(set1);
+        Set<String> result = new HashSet<>();
+        for (String s : set2) {
+            if (!temp.add(s)) {
+                result.add(s);
+            }
+        }
+        return result;
     }
 }
 
